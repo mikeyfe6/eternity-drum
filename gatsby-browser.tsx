d2e7add4d0be2e9520be2ef4bab201287d6eb4d2
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2023-10-24 23:45:25 +0200

    scroll to top when navig. pages

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,12 +1,7 @@
 export const onServiceWorkerUpdateReady = () => window.location.reload();
 
-// * Met confirm optie is hier beneden....
-// export const onServiceWorkerUpdateReady = () => {
-// 	const answer = window.confirm(
-// 		`De webapp heeft updates! ` + `Herladen om de nieuwste versie te zien?`
-// 	);
-
-// 	if (answer === true) {
-// 		window.location.reload();
-// 	}
-// };
+export const onRouteUpdate = ({ location, prevLocation }) => {
+	if (location !== prevLocation) {
+		window.scrollTo(0, 0);
+	}
+};
