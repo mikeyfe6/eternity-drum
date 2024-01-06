80df7602f175032c6d588b90a2c8320b00a20e38
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-01-06 04:56:02 +0100

    fixed heroimages crash on mobile & swiper improvs

--- src/components/heroslider.tsx
+++ src/components/heroslider.tsx
@@ -61,44 +61,54 @@ const Hero: React.FC = () => {
 				className={styles.swiperWrapper}
 			>
 				<SwiperSlide className={styles.swiperSlide}>
-					<StaticImage
-						src='../images/hero/beats.jpg'
-						alt={`Beats`}
-						placeholder='blurred'
-						className={styles.swiperImage}
-					/>
+					<div className={styles.swiperImage}>
+						<StaticImage
+							src='../images/hero/beats.jpg'
+							alt={`Beats`}
+							placeholder='blurred'
+							style={{ width: '100%' }}
+						/>
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className={styles.swiperSlide}>
-					<StaticImage
-						src='../images/hero/manhood-ac.jpg'
-						alt={`Manhood`}
-						placeholder='blurred'
-						className={styles.swiperImage}
-					/>
+					<div className={styles.swiperImage}>
+						<StaticImage
+							src='../images/hero/manhood-ac.jpg'
+							alt={`Manhood`}
+							placeholder='blurred'
+							style={{ width: '100%' }}
+						/>
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className={styles.swiperSlide}>
-					<StaticImage
-						src='../images/hero/umuntu.jpg'
-						alt={`Umuntu`}
-						placeholder='blurred'
-						className={styles.swiperImage}
-					/>
+					<div className={styles.swiperImage}>
+						<StaticImage
+							src='../images/hero/umuntu.jpg'
+							alt={`Umuntu`}
+							placeholder='blurred'
+							style={{ width: '100%' }}
+						/>
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className={styles.swiperSlide}>
-					<StaticImage
-						src='../images/hero/young-ep.jpg'
-						alt={`Young Eternity`}
-						placeholder='blurred'
-						className={styles.swiperImage}
-					/>
+					<div className={styles.swiperImage}>
+						<StaticImage
+							src='../images/hero/young-ep.jpg'
+							alt={`Young Eternity`}
+							placeholder='blurred'
+							style={{ width: '100%' }}
+						/>
+					</div>
 				</SwiperSlide>
 				<SwiperSlide className={styles.swiperSlide}>
-					<StaticImage
-						src='../images/hero/thecrew.jpg'
-						alt={`The Crew`}
-						placeholder='blurred'
-						className={styles.swiperImage}
-					/>
+					<div className={styles.swiperImage}>
+						<StaticImage
+							src='../images/hero/thecrew.jpg'
+							alt={`The Crew`}
+							placeholder='blurred'
+							style={{ width: '100%' }}
+						/>
+					</div>
 				</SwiperSlide>
 
 				<div className={styles.autoplayProgress} slot='container-end'>
