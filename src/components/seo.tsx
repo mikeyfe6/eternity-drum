fd0d9e872951a298c42c8d144db1338923ce45c3
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-01-16 00:12:13 +0100

    updated packages + more..

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -64,7 +64,10 @@ export const Seo: React.FC<SEOProps> = ({
 			{/* ICONS */}
 			<link
 				rel='stylesheet'
-				href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+				href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+				integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
+				crossOrigin='anonymous'
+				referrerPolicy='no-referrer'
 			/>
 
 			{children}
